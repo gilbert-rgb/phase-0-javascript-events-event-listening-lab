e84957eb6014a31df4b399d971b1c1c17f07cd53
Gilbert Icheboi <gilbert.icheboi@student.moringaschool>
2025-03-02 21:54:01 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+
 }
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+      console.log('Event listener works!');
+    });
+  }
+  addingEventListener();
+  
